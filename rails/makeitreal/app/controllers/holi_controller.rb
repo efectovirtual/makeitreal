c917c760225fb9409b3f410ab4b018a494edc9eb
sescobb27 <s.escob.b.27@gmail.com>
2017-05-31 21:45:21 -0500

    [add] layouts, partials and route aliases

--- rails/makeitreal/app/controllers/holi_controller.rb
+++ rails/makeitreal/app/controllers/holi_controller.rb
@@ -1,12 +1,19 @@
 class HoliController < ApplicationController
+  layout ''
   def crear
+    # redirect_to holi_actualizar_path
+    redirect_to yepeto_path(name: 'simon')
   end
 
   def actualizar
+    # redirect_to products_path
+    redirect_to products_url
   end
 
   def ver
     @name = params[:name]
+    @products = ['Mac', 'Pc', 'Guaro']
+    render layout: 'custom_products'
   end
 
   def products
--- rails/makeitreal/config/routes.rb
+++ rails/makeitreal/config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   get 'holi/crear'
 
   get 'holi/actualizar'
-  get 'holi/products'
+  get 'holi/products', as: 'products'
 
-  get 'holi/ver(/:name)', to: 'holi#ver'
+  get 'holi/ver(/:name)', to: 'holi#ver', as: 'yepeto'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get 'index', to: 'index#holi'
